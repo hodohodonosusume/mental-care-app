8cf7227c3d4f3e1e331531b883f65af721f1aa30
hodohodonosusume <hodohodonosusume@gmail.com>
2025-07-08 15:39:15 +0900

    feat: Implement main screen with bottom navigation

---
+++ lib/screens/self_check_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SelfCheckScreen extends StatelessWidget {
+  const SelfCheckScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'ここはセルフチェック画面です',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
